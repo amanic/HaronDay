a5cc5ad74ade8619efb494c29d4f80448c233dd1
amanic <1642485710@qq.com>
2018-07-27 15:58:33 +0800

    wx拦截器

--- webapp/src/main/java/com/haron/pro/haron/controller/TetsController.java
+++ webapp/src/main/java/com/haron/pro/haron/controller/TetsController.java
@@ -1,5 +1,6 @@
 package com.haron.pro.haron.controller;
 
+import com.haron.pro.common.util.WxWebUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,11 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by chenhaitao on 2018/7/26.
  */
 @RestController
-@RequestMapping("test")
+@RequestMapping("wx/test")
 public class TetsController {
 
     @GetMapping("t1")
     public String t1(){
         return "hello~";
+    }
+
+
+    @GetMapping("t2")
+    public String t2(){
+        return "token是："+WxWebUtils.getWxWebUserFromSession().getOpenId();
     }
 }
