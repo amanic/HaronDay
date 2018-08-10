41b2f47b7b70294f2daaca2891d84d913f1130c2
amanic <1642485710@qq.com>
2018-08-10 10:46:25 +0800

    ip

--- webapp/src/main/java/com/haron/pro/haron/aop/OperationLogAop.java
+++ webapp/src/main/java/com/haron/pro/haron/aop/OperationLogAop.java
@@ -44,37 +44,7 @@ public class OperationLogAop {
             log.error("请求为空");
         } else {
             HttpServletRequest request = attributes.getRequest();
-            String Xip = request.getHeader("X-Real-IP");
-            String XFor = request.getHeader("X-Forwarded-For");
-            if(StringUtils.isNotEmpty(XFor) && !"unKnown".equalsIgnoreCase(XFor)){
-                //多次反向代理后会有多个ip值，第一个ip才是真实ip
-                int index = XFor.indexOf(",");
-                if(index != -1){
-                    return XFor.substring(0,index);
-                }else{
-                    return XFor;
-                }
-            }
-            XFor = Xip;
-            if(StringUtils.isNotEmpty(XFor) && !"unKnown".equalsIgnoreCase(XFor)){
-                return XFor;
-            }
-            if (StringUtils.isBlank(XFor) || "unknown".equalsIgnoreCase(XFor)) {
-                XFor = request.getHeader("Proxy-Client-IP");
-            }
-            if (StringUtils.isBlank(XFor) || "unknown".equalsIgnoreCase(XFor)) {
-                XFor = request.getHeader("WL-Proxy-Client-IP");
-            }
-            if (StringUtils.isBlank(XFor) || "unknown".equalsIgnoreCase(XFor)) {
-                XFor = request.getHeader("HTTP_CLIENT_IP");
-            }
-            if (StringUtils.isBlank(XFor) || "unknown".equalsIgnoreCase(XFor)) {
-                XFor = request.getHeader("HTTP_X_FORWARDED_FOR");
-            }
-            if (StringUtils.isBlank(XFor) || "unknown".equalsIgnoreCase(XFor)) {
-                XFor = request.getRemoteAddr();
-            }
-            opLog.setIpAddress(XFor);
+            opLog.setIpAddress(getIpAdrress(request));
             log.info("请求Request = method->{},\npathinfo->{},\ncontextPath->{},\nrequestURI->{},\nservletContext->{}," +
                             "\nauthType->{},\ncookies->{},\nremoteUser->{},\nservletPath->{}," +
                             "\nserverName->{},\nparameterMap->{},\nremoteAddr->{},\nrequestURL->{}",
@@ -94,7 +64,6 @@ public class OperationLogAop {
                     request.getRequestURL());
             log.info("*********"+request.toString());
         }
-
         Object[] args = pjp.getArgs();
         if (args == null || args.length == 0) {
             opLog.setParam("没有参数");
@@ -122,4 +91,38 @@ public class OperationLogAop {
         return o;
     }
 
+
+    private static String getIpAdrress(HttpServletRequest request) {
+        String Xip = request.getHeader("X-Real-IP");
+        String XFor = request.getHeader("X-Forwarded-For");
+        if(StringUtils.isNotEmpty(XFor) && !"unKnown".equalsIgnoreCase(XFor)){
+            //多次反向代理后会有多个ip值，第一个ip才是真实ip
+            int index = XFor.indexOf(",");
+            if(index != -1){
+                return XFor.substring(0,index);
+            }else{
+                return XFor;
+            }
+        }
+        XFor = Xip;
+        if(StringUtils.isNotEmpty(XFor) && !"unKnown".equalsIgnoreCase(XFor)){
+            return XFor;
+        }
+        if (StringUtils.isBlank(XFor) || "unknown".equalsIgnoreCase(XFor)) {
+            XFor = request.getHeader("Proxy-Client-IP");
+        }
+        if (StringUtils.isBlank(XFor) || "unknown".equalsIgnoreCase(XFor)) {
+            XFor = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (StringUtils.isBlank(XFor) || "unknown".equalsIgnoreCase(XFor)) {
+            XFor = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (StringUtils.isBlank(XFor) || "unknown".equalsIgnoreCase(XFor)) {
+            XFor = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (StringUtils.isBlank(XFor) || "unknown".equalsIgnoreCase(XFor)) {
+            XFor = request.getRemoteAddr();
+        }
+        return XFor;
+    }
 }
