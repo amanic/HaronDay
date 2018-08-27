8b90e2a597322842e3a6d329a08f991442d72851
martea <xiyaode@yuwoWY>
2018-08-27 14:28:50 +0800

    静态资源配置访问3

--- webapp/src/main/java/com/haron/pro/haron/config/FileShowConfig.java
+++ webapp/src/main/java/com/haron/pro/haron/config/FileShowConfig.java
@@ -1,20 +1,20 @@
-//package com.haron.pro.haron.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-//
-///**
-// * Created by martea on 2018/8/27.
-// */
-//@Configuration
-//public class FileShowConfig extends WebMvcConfigurerAdapter {
-//
-//
-//
-//        @Override
-//        public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//            //和页面有关的静态目录都放在项目的static目录下
-//            registry.addResourceHandler("/temp/**").addResourceLocations("file:/usr/local/tomcat/haron/");
-//        }
-//}
+package com.haron.pro.haron.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by martea on 2018/8/27.
+ */
+@Configuration
+public class FileShowConfig extends WebMvcConfigurerAdapter {
+
+
+
+        @Override
+        public void addResourceHandlers(ResourceHandlerRegistry registry) {
+            //和页面有关的静态目录都放在项目的static目录下
+            registry.addResourceHandler("/temp/**").addResourceLocations("file:/usr/local/tomcat/haron/");
+        }
+}
