e62aa95241ef33b6d17e673f84387b396c8379b4
martea <xiyaode@yuwoWY>
2018-08-28 16:00:21 +0800

    尝试展示相册2

--- service/src/main/java/com/haron/pro/service/impl/AlbumServiceImpl.java
+++ service/src/main/java/com/haron/pro/service/impl/AlbumServiceImpl.java
@@ -45,7 +45,7 @@ public class AlbumServiceImpl implements AlbumService{
                 String fileContent = FileUtil.fileRead("/usr/local/tomcat/haron/albumExample/view/index.html");
                 fileContent.replaceAll("￥",wxUser.getNickName());
                 String albumString = FileUtil.albumString.replaceAll("￥",wxRequest.getBody().getPicUrl());
-                fileContent.replaceAll("%",albumString);
+                fileContent.replaceAll("albumReplacement",albumString);
                 FileUtil.WriteStringToFile(fileName,fileContent);
             }
             return WxMessage.textBuilder().content("上传成功，图片链接为："+wxRequest.getBody().getPicUrl()+",后续将推出纪念日相册功能，敬请期待！").toGroup(wxUser.getOpenId()).build();
